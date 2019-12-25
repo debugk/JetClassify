27933f4a3805931863932a95f076598eaafc25f8
Fudong He <fudong.he@cern.ch>
2019-12-25 13:47:49 +0800

    update

--- macros/plotInput.py
+++ macros/plotInput.py
@@ -10,6 +10,7 @@ import time
 import h5py
 import numpy as np
 import keras
+import pandas as pd
 
 import matplotlib
 
@@ -17,20 +18,10 @@ from optparse import OptionParser
 p = OptionParser()
 
 p.add_option('--outdir', '-o',   type='string',        default=None)
-p.add_option('--key',    '-k',   type='string',        default=None)
-p.add_option('--min-nevent',     type='int',           default=400)
-p.add_option('--nclass',         type='int',           default=3)
+p.add_option('--outname',        type='string',        default='new_train_jet.csv')
 
 p.add_option('--debug', '-d',    action='store_true',  default=False)
-p.add_option('--batch', '-b',    action='store_true',  default=False)
-p.add_option('--show',  '-s',    action='store_true',  default=False)
-p.add_option('--show-extra',     action='store_true',  default=False)
-p.add_option('--predict',        action='store_true',  default=False)
-
-p.add_option('--do-all-rocs',    action='store_true',  default=False)
-p.add_option('--no-roc',         action='store_true',  default=False)
-p.add_option('--no-acc',         action='store_true',  default=False)
-p.add_option('--no-rnn',         action='store_true',  default=False)
+p.add_option('--do-add',         action='store_true',  default=False)
 
 (options,args) = p.parse_args()
 
@@ -133,6 +124,23 @@ def getVarBinsMinMax(var_name):
 
     return result
 
+#======================================================================================================        
+def getDataFrameRange(df_vars):
+    
+    var_max = None
+    var_min = None
+
+    for df_var in df_vars:
+        if not var_max or df_var.max() > var_max:
+            var_max = df_var.max()
+
+        if not var_min or df_var.min() < var_min:
+            var_min = df_var.min()
+
+    return (var_max, var_min)
+
+#======================================================================================================
+# make plots
 #======================================================================================================
 def plotInputVars(fname):
 
@@ -164,9 +172,215 @@ def plotInputVars(fname):
         plt.grid(True)
         saveFig(plt, '%s.pdf' %(var_name))
 
-    if options.show:
-        plt.show()
+#======================================================================================================
+def plotInterestVars(fname):
 
+    ''' Plot interesting variables '''
+
+    #
+    # Pandas is a useful tool !
+    #
+    ifile  = pd.read_csv(fname)
+    labels = ifile['label']
+
+    plt.clf()
+    phd = plt.hist(labels, range=(0, 22), bins=22, histtype='step', color='red', label='label', density=True)
+    plt.grid(True)
+
+    saveFig(plt, '%s.pdf' %('label_all'))
+
+    #
+    # get 4 class indexes refs
+    #
+    class_d = ifile['label'] == 1
+    class_c = ifile['label'] == 4
+    class_b = ifile['label'] == 5
+    class_g = ifile['label'] == 21
+    
+    class_dict = {1:class_d,
+                  4:class_c,
+                  5:class_b,
+                  21:class_g
+                  }
+    # 
+    # Plot pT 
+    # 
+    for var_name in ['jet_p3', 'jet_theta_x', 'jet_theta_y', 'jet_theta_z']:
+    #for var_name in ['jet_pt_x', 'jet_pt_y', 'jet_phi_x', 'jet_phi_y', 'jet_phi_z']:
+    #for var_name in ['jet_pt', 'jet_sin_theta', 'jet_tan_phi']:
+        plt.clf()
+        
+        var_class_4 = []
+
+        for label in [1, 4, 5, 21]:
+            var_class_4 += [ifile[class_dict[label] ][var_name] ]
+
+        var_max, var_min = getDataFrameRange(var_class_4)
+
+        for label in [1, 4, 5, 21]: 
+            class_name  = getClassName(label) 
+            class_color = getClassColor(class_name) 
+            select_data = ifile[class_dict[label] ][var_name]
+            
+            phd = plt.hist(select_data.values, range=(var_min, var_max), bins=100, histtype='step', color=class_color, label=class_name, density=True)
+ 
+        plt.autoscale(enable=True, axis='y', tight=None)
+        plt.legend(loc='upper right')
+        plt.xlabel(var_name)
+        plt.ylabel('Density')
+        plt.grid(True)
+        saveFig(plt, '%s.pdf' %(var_name))
+      
+    # Save the new DataFrame
+    ifile.to_csv('new_train_jet.csv', index=False)
+
+
+#======================================================================================================
+def checkSameEventID(fname):
+
+    ''' Plot interesting variables '''
+
+    ifile  = pd.read_csv(fname)
+    
+    id_sort   = ifile.sort_values(by='event_id').drop_duplicates(subset=['event_id'])['event_id']
+    id_labels = ifile[['event_id','label']]
+    
+    for evt_id in id_sort.values:
+        init_label = -1
+
+        subset_same_evtid = id_labels[id_labels['event_id'] == evt_id]['label'].values
+
+        for label in subset_same_evtid:
+            if init_label < 0:
+                init_label = label
+            elif init_label != label:
+                print('ERROR: find the event id = %s with different label jet.'%evt_id)
+
+        print('INFO: all jet in the event id = %s with same label = %d'%(evt_id, init_label) )
+    
+
+#======================================================================================================
+# variable processing
+#======================================================================================================
+def jet_pt(df):
+    px = df['jet_px']
+    py = df['jet_py']
+
+    return (px**2+py**2)**0.5
+
+#======================================================================================================
+def jet_pt_y(df):
+    px = df['jet_px']
+    pz = df['jet_pz']
+
+    return (px**2+pz**2)**0.5
+
+#======================================================================================================
+def jet_pt_x(df):
+    py = df['jet_py']
+    pz = df['jet_pz']
+
+    return (py**2+pz**2)**0.5
+
+#======================================================================================================
+def jet_energy(df):
+    px = df['jet_px']
+    py = df['jet_py']
+    pz = df['jet_pz']
+    m  = df['jet_mass']
+    return (px**2 + py**2 + pz**2 + m**2)**0.5
+
+#======================================================================================================
+def jet_sin_theta(df):
+    # cos theta 
+    px = df['jet_px']
+    py = df['jet_py']
+    pz = df['jet_pz']
+    return pz/(px**2 + py**2 + pz**2)**0.5
+
+#======================================================================================================
+def jet_p3(df):
+    # cos theta 
+    px = df['jet_px']
+    py = df['jet_py']
+    pz = df['jet_pz']
+    return (px**2 + py**2 + pz**2)**0.5
+
+
+#======================================================================================================
+def jet_theta_x(df):
+    px = df['jet_px']
+    p3 = df['jet_p3']
+
+    return math.acos(px/p3)
+
+#======================================================================================================
+def jet_theta_y(df):
+    py = df['jet_py']
+    p3 = df['jet_p3']
+
+    return math.acos(py/p3)
+
+#======================================================================================================
+def jet_theta_z(df):
+    pz = df['jet_pz']
+    p3 = df['jet_p3']
+
+    return math.acos(pz/p3)
+
+#======================================================================================================
+def jet_tan_phi(df):
+    px = df['jet_px']
+    py = df['jet_py']
+    return py/px
+
+#======================================================================================================
+def jet_phi_z(df):
+    px = df['jet_px']
+    py = df['jet_py']
+    return math.atan(py/px)
+
+#======================================================================================================
+def jet_phi_y(df):
+    px = df['jet_px']
+    pz = df['jet_pz']
+    return math.atan(pz/px)
+
+#======================================================================================================
+def jet_phi_x(df):
+    py = df['jet_py']
+    pz = df['jet_pz']
+    return math.atan(pz/py)
+
+#======================================================================================================
+def AddNewVarsToFile(fname):
+
+    ''' Add interesting variables '''
+
+    # 
+    # Add pT 
+    #
+    ifile  = pd.read_csv(fname)
+
+   # ifile['jet_pt_y'] = ifile.apply(jet_pt_y,     axis = 1)  # axis = 1 mean process on column; = 0 is on row
+    ifile['jet_pt_x'] = ifile.apply(jet_pt_x,     axis = 1)  # axis = 1 mean process on column; = 0 is on row
+
+   # ifile['jet_phi_x'] = ifile.apply(jet_phi_x,    axis = 1)  
+   # ifile['jet_phi_y'] = ifile.apply(jet_phi_y,    axis = 1)  
+   # ifile['jet_phi_z'] = ifile.apply(jet_phi_z,    axis = 1)  
+
+    ifile['jet_p3'] = ifile.apply(jet_p3, axis = 1) # must add before theta calculation 
+   
+    ifile['jet_theta_x'] = ifile.apply(jet_theta_x, axis = 1)  
+    #ifile['jet_theta_y'] = ifile.apply(jet_theta_y, axis = 1)  
+    #ifile['jet_theta_z'] = ifile.apply(jet_theta_z, axis = 1)  
+    
+    # ifile['jet_energy2'] = ifile.apply(jet_energy, axis = 1)  
+    #   -- find that the jet_energy2 == jet_energy
+      
+    # Save the new DataFrame
+    out_name = getOutName(options.outname)
+    ifile.to_csv(out_name, index=False)
 
 #======================================================================================================
 def main():
@@ -181,8 +395,14 @@ def main():
         log.warning('Input file does not exist: %s' %fname)
         return    
 
-    plotInputVars(fname)      
-  
+    #plotInputVars(fname)      
+
+    if options.do_add:
+        AddNewVarsToFile(fname)
+    
+    else:
+        checkSameEventID(fname)
+        #plotInterestVars(fname)
 
 #======================================================================================================
 if __name__ == '__main__':
--- macros/trainJetClassify.py
+++ macros/trainJetClassify.py
@@ -9,6 +9,7 @@ import logging
 import h5py
 import numpy as np
 import keras
+import pandas as pd
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -20,8 +21,11 @@ from optparse import OptionParser
 p = OptionParser()
 
 p.add_option('-o', '--outdir',   type='string',                      default='models/')
+p.add_option('-t', '--testfile', type='string',                      default=None)
+p.add_option('--njet',           type='int',                         default=6)
 p.add_option('-n', '--nevent',   type='int',                         default=None)
 p.add_option('-d', '--debug',    action='store_true',  dest='debug', default=False)
+
 
 (options,args) = p.parse_args()
 
@@ -76,21 +80,18 @@ def countEvents(labels, select_label):
 def oneHotLabel(var_labels):
 
     one_hot_labels = []
+    one_hot_dict   = {1:[1, 0, 0, 0],
+                      4:[0, 1, 0, 0],
+                      5:[0, 0, 1, 0],
+                      21:[0, 0, 0, 1]
+                     }
 
     for i in var_labels:
-        if i == 1:
-            one_hot = [1, 0, 0, 0]
-        elif i == 4:
-            one_hot = [0, 1, 0, 0]
-        elif i == 5:
-            one_hot = [0, 0, 1, 0]
-        else:
-            one_hot = [0, 0, 0, 1]
 
         if len(one_hot_labels) == 0:
             one_hot_labels = np.array(one_hot)
         else:
-            one_hot_labels = np.vstack((one_hot_labels, np.array(one_hot)))
+            one_hot_labels = np.vstack((one_hot_labels, np.array(one_hot_dict[i])))
 
     return one_hot_labels
 
@@ -98,21 +99,23 @@ def oneHotLabel(var_labels):
 #======================================================================================================        
 def oneHotLabelFast(var_labels):
 
-    one_hot_labels = []
+    one_hot_labels = [] 
+    one_hot_dict = {1:0, 4:1, 5:2, 21:3} 
 
     for i in var_labels:
-        if i == 1:
-            one_hot = 0 
-        elif i == 4:
-            one_hot = 1
-        elif i == 5:
-            one_hot = 2
-        else:
-            one_hot = 3
 
-        one_hot_labels += [one_hot]
+        one_hot_labels += [one_hot_dict[i]]
 
     return np.array(one_hot_labels)
+
+#======================================================================================================
+def sub_process(x):
+
+    out_dict = {0:1, 1:4, 2:5, 3:21}
+
+    x = out_dict[x]
+
+    return x
 
 #======================================================================================================
 def getOutName(name):
@@ -127,47 +130,199 @@ def getOutName(name):
 
     return '%s/%s' %(outdir.rstrip('/'), name)
 
+#======================================================================================================
+def trainRNN(train_data, train_labels):
+
+    '''Train RNN algorithm'''
+
+    timeStart = time.time()
+
+    train_labels_bin = keras.utils.to_categorical(oneHotLabelFast(train_labels))
+
+    log.info('trainRNN - start')
+    log.info('   train_data       len=%s, shape=%s, dtype=%s' %(len(train_data),       train_data      .shape, train_data      .dtype))
+    log.info('   train_labels     len=%s, shape=%s, dtype=%s' %(len(train_labels),     train_labels    .shape, train_labels    .dtype))
+    log.info('   train_labels_bin len=%s, shape=%s, dtype=%s' %(len(train_labels_bin), train_labels_bin.shape, train_labels_bin.dtype))
+
+    log.info('Will configure model...')
+
+    timeSteps = train_data.shape[1] # number of jets
+    nFeatures = train_data.shape[2] # number of variables per jets
+
+    log.info('trainRNN - number of time steps (tracks):          %d' %timeSteps)
+    log.info('trainRNN - number of features   (track variables): %d' %nFeatures)
+
+    #-----------------------------------------------------------------------------------
+    # Create and connect graphs
+    #
+    jet_inputs = keras.layers.Input(shape=(timeSteps, nFeatures), name="jets_inputs")
+
+    masked_input = keras.layers.Masking()(jet_inputs)
+
+    log.info('jet_inputs shape=%s' %jet_inputs.shape)
+    
+    lstm = keras.layers.LSTM(10, return_sequences=False, name='LSTM')(masked_input)
+
+    dpt = keras.layers.Dropout(rate=0.1)(lstm)
+
+    FC = keras.layers.Dense(10, activation='relu', name="Dense")(dpt)
+
+    output = keras.layers.Dense(4, activation='softmax', name="jet_class")(FC)
+
+    log.info('Will create model...')
+
+    model = keras.models.Model(inputs=jet_inputs, outputs=output)
+
+    log.info('Will compile model...')
+
+    #-----------------------------------------------------------------------------------
+    # Compile and fit model
+    #
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+    model.summary()
+
+    csv_logger = keras.callbacks.CSVLogger(getOutName('train_RNN_loss.csv'))
+
+    log.info('Will fit model...')
+
+    fhist = model.fit(train_data,
+                      train_labels_bin,
+                      epochs=10,
+                      batch_size=256,
+                      callbacks=[csv_logger])
+
+    log.info(str(fhist))
+
+    model.summary()
+
+    log.info('trainRNN - all done in %.1f seconds' %(time.time()-timeStart))
+
+    return model
+
+
 #======================================================================================================        
 def trainMLP(train_data, train_labels, title):
 
     '''Train simple MLP algorithm'''
-    #train_labels_bin = oneHotLabel(train_labels)
-    #test_labels_bin  = oneHotLabel(test_labels)
     train_labels_bin = keras.utils.to_categorical(oneHotLabelFast(train_labels))
 
     log.info('trainMLP - with title=%s' %(title))
-    log.info('   train_data   len=%s, shape=%s, dtype=%s'     %(len(train_data),       train_data  .shape,     train_data  .dtype))
-    log.info('   train_labels len=%s, shape=%s, dtype=%s'     %(len(train_labels),     train_labels.shape,     train_labels.dtype))    
-    log.info('   train_labels_bin len=%s, shape=%s, dtype=%s' %(len(train_labels_bin), train_labels_bin.shape, train_labels_bin.dtype))    
+    log.info('   train_data   len=%s, shape=%s'     %(len(train_data),       train_data  .shape))
+    log.info('   train_labels len=%s, shape=%s'     %(len(train_labels),     train_labels.shape))    
+    log.info('   train_labels_bin len=%s, shape=%s' %(len(train_labels_bin), train_labels_bin.shape))    
 
     log.info('Will configure model...')
     
+    ndim = len(train_data.keys()) 
+    
     model = Sequential()
-    model.add(Dense(64, input_dim=6, activation='relu'))
-    #model.add(Dropout(0.5))
+    model.add(Dense(64, input_dim=ndim, activation='relu'))
+    model.add(Dropout(0.1))
     model.add(Dense(64, activation='relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(4, activation='sigmoid'))
+    model.add(Dropout(0.1))
+    model.add(Dense(10, activation='sigmoid'))
+    model.add(Dense(4, activation='softmax'))
 
     log.info('Will compile model...')
     
     csv_logger = keras.callbacks.CSVLogger(getOutName('train_DNN_loss.csv'))
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
                   metrics=['accuracy'])
 
     log.info('Will fit model...')
     
     model.fit(train_data, train_labels_bin,
               validation_split=0.1,
-              epochs=100,
+              epochs=40,
+              batch_size=10000,
               callbacks=[csv_logger])
 
     return model
 
+
 #======================================================================================================        
-def saveModel(model, train_data=[], test_data=[], lfile = None):
+def savePrediction(model, test_data=[], lfile = None):
+
+    outkey = "first_DNN"
+
+    if outkey == None:
+        return
+
+    fname_pred   = getOutName('%s_predictions.csv' %outkey)
+
+    if model == None:
+        #   
+        # Just check that output files do not already exist
+        #   
+        for f in [fname_model, fname_weight, fname_arch, fname_pred]:
+            if os.path.isfile(f):
+                raise Exception('saveModel - file already exists: \"%s\"' %f) 
+
+        return 
+
+    log.info('saveModel - save model predictions for test and training data')
+
+    pdata = model.predict(test_data)
+
+    # WITH PANDAS
+    #  -- clear and easy implement
+    sub=pd.DataFrame() # Data format of Pandas
+    sub['id']    = lfile['jet_id']
+    sub['label'] = list(pdata.argmax(axis=1))
+    sub['label'] = sub['label'].apply(sub_process)
+    
+    sub.to_csv(fname_pred, index=False) 
+     
+    # SAVE PREDICT TO TXT
+    #    np.savetxt(fname_pred, pdata, fmt='%.18e',delimiter=None)
+    #
+
+    # IF NO PANDAS
+    #    out_dict = {0:1,
+    #                1:4,
+    #                2:5,
+    #                3:21
+    #                }
+    #
+    #    with open(fname_pred, 'w') as f:
+    #         for pdict in pdata:
+    #             max_index = np.argmax(pdict) 
+    #             f.write('%d\n'%out_dict[max_index])
+
+
+#======================================================================================================        
+def saveRNNPrediction(model, id_list, out_test_data, evtid_jetid_dict):
+
+    outkey = "first_RNN"
+
+    if outkey == None:
+        return
+
+    fname_pred   = getOutName('%s_predictions.csv' %outkey)
+
+    pdata = model.predict(out_test_data).argmax(axis=1)
+    
+    if len(pdata) != len(id_list):
+        log.error('saveRNNPrediction - Wrong number of pridictions')
+        return
+
+    with open(fname_pred, 'w') as f:
+        f.write('id,label\n')
+
+        for i in range(len(id_list)):
+            evt_id = id_list.values[i]
+
+            label = sub_process(pdata[i])
+
+            for jet_id in evtid_jetid_dict[evt_id]:
+                f.write('%s,%d'%(jet_id, label))
+
+
+#======================================================================================================        
+def saveModel(model, train_data=[]):
 
     outkey = "first_DNN"
 
@@ -177,13 +332,12 @@ def saveModel(model, train_data=[], test_data=[], lfile = None):
     fname_model  = getOutName('%s_model.h5'        %outkey)
     fname_weight = getOutName('%s_weights.h5'      %outkey)
     fname_arch   = getOutName('%s_arch.json'       %outkey)
-    fname_pred   = getOutName('%s_predictions.csv' %outkey)
 
     if model == None:
         #   
         # Just check that output files do not already exist
         #   
-        for f in [fname_model, fname_weight, fname_arch, fname_pred]:
+        for f in [fname_model, fname_weight, fname_arch]:
             if os.path.isfile(f):
                 raise Exception('saveModel - file already exists: \"%s\"' %f) 
 
@@ -202,25 +356,84 @@ def saveModel(model, train_data=[], test_data=[], lfile = None):
     log.info('saveModel - saved weights to:      %s' %fname_weight)
     log.info('saveModel - saved architecture to: %s' %fname_arch)
 
-    log.info('saveModel - save model predictions for test and training data')
-
-    pdata = model.predict(test_data)
-    
-    #np.savetxt(fname_pred, pdata, fmt='%.18e',delimiter=None)
-
-    out_dict = {0:1,
-                1:4,
-                2:5,
-                3:21
-                }
-
-    with open(fname_pred, 'w') as f:
-         for pdict in pdata:
-             max_index = np.argmax(pdict)
-             f.write('%d\n'%out_dict[max_index])
 
 #======================================================================================================        
-def main():
+def main_trainRNN():
+
+    if len(args) != 1:
+        log.warning('Wrong number of command line arguments: %s' %len(args))
+        return
+
+    fname = args[0]
+
+    if not os.path.isfile(fname):
+        log.warning('Input file does not exist: %s' %fname)
+        return    
+    
+    train_file = pd.read_csv(fname)
+    
+    # prepare training data for RNN
+    input_var_names = ['number_of_particles_in_this_jet', 'jet_px', 'jet_py', 'jet_pz', 'jet_energy', 'jet_mass', 'jet_theta_x', 'jet_pt_x']
+    id_list = train_file.drop_duplicates(subset=['event_id'])['event_id']
+    nevt    = len(id_list)
+
+    print("number of events = ", nevt)
+
+    out_train_data  = np.zeros((nevt, options.njet, len(input_var_names)))
+    out_train_label = np.zeros((nevt))
+
+    for i in range(nevt):
+        evt_id = id_list.values[i]
+        is_same_evtid = train_file['event_id'] == evt_id
+        
+        subset_train = train_file[is_same_evtid][input_var_names].values
+        subset_label = train_file[is_same_evtid]['label'].values[0]
+        
+        njet = subset_train.shape[0]
+
+        out_train_label[i] = subset_label
+
+        for j in range(njet):
+            if j <= options.njet:
+                out_train_data[i][j] = subset_train[j]
+
+    model = trainRNN(out_train_data, out_train_label)
+
+    saveModel(model, out_train_data)
+
+    if options.testfile and  os.path.isfile(options.testfile):
+        test_file = pd.read_csv(options.testfile)
+
+        id_list = test_file.drop_duplicates(subset=['event_id'])['event_id']
+        nevt    = len(id_list)
+
+        print("number of test events = ", nevt)
+
+        out_test_data  = np.zeros((nevt, options.njet, len(input_var_names)))
+
+        evtid_data_dict  = {}
+        evtid_jetid_dict = {}
+
+        for i in range(nevt):
+            evt_id = id_list.values[i]
+            is_same_evtid = test_file['event_id'] == evt_id
+            
+            subset_test      = test_file[is_same_evtid][input_var_names].values
+
+            njet = subset_test.shape[0]
+
+            for j in range(njet):
+                if j <= options.njet:
+                    out_test_data[i][j] = subset_test[j]
+            
+            evtid_data_dict[evt_id]  = out_test_data[i]
+            evtid_jetid_dict[evt_id] = test_file[is_same_evtid]['jet_id'].values
+
+        saveRNNPrediction(model, id_list, out_test_data, evtid_jetid_dict)
+ 
+
+#======================================================================================================        
+def main_trainMLP():
 
     if len(args) != 1:
         log.warning('Wrong number of command line arguments: %s' %len(args))
@@ -232,22 +445,31 @@ def main():
         log.warning('Input file does not exist: %s' %fname)
         return    
   
-    # 
-    # Get the csv input file
+    #===================================================================================================
+    # IF NO PANDAS IMPORTED: 
+    #   You can get the csv input file
+    #    
+    #    train_file = np.genfromtxt(fname, dtype=float, delimiter=',', names=True)
     #
-    ifile = np.genfromtxt(fname, dtype=float, delimiter=',', names=True)
+    #    all_data = []
+    #
+    #    for var_name in ['number_of_particles_in_this_jet', 'jet_px', 'jet_py', 'jet_pz', 'jet_energy', 'jet_mass']:
+    #        if len(all_data) == 0:
+    #            all_data = train_file[var_name]
+    #        else:
+    #            all_data = np.vstack((all_data, train_file[var_name]))
+    #
+    #    all_data = all_data.T
+    #===================================================================================================
+    
+    train_file = pd.read_csv(fname)
+    input_var_names = ['number_of_particles_in_this_jet', 'jet_pt_x', 'jet_theta_x', 'jet_p3', 'jet_energy', 'jet_mass']
+    #input_var_names = ['number_of_particles_in_this_jet', 'jet_pt', 'jet_sin_theta', 'jet_tan_phi', 'jet_pz', 'jet_energy', 'jet_mass']
+    #input_var_names = ['number_of_particles_in_this_jet', 'jet_px', 'jet_py', 'jet_pz', 'jet_energy', 'jet_mass']
 
-    all_data = []
+    all_data   = train_file[input_var_names]
 
-    for var_name in ['number_of_particles_in_this_jet', 'jet_px', 'jet_py', 'jet_pz', 'jet_energy', 'jet_mass']:
-        if len(all_data) == 0:
-            all_data = ifile[var_name]
-        else:
-            all_data = np.vstack((all_data, ifile[var_name]))
-
-    all_data = all_data.T
-
-    all_labels = ifile['label']
+    all_labels = train_file['label']
 
     train_data   = all_data[:]    
     train_labels = all_labels[:]  
@@ -258,23 +480,13 @@ def main():
     log.info('Select indices for test events...')
 
     model = trainMLP(train_data, train_labels, 'MLP')  
-    
-    lfile = np.genfromtxt('tem_train.csv', dtype=float, delimiter=',', names=True)
-    #lfile = np.genfromtxt('tem_test_1000.csv', dtype=float, delimiter=',', names=True)
-    #lfile = np.genfromtxt('jet_simple_data/simple_test_R04_jet.csv', dtype=float, delimiter=',', names=True)
+    saveModel(model, train_data)
 
-    all_test_data = []
-
-    for var_name in ['number_of_particles_in_this_jet', 'jet_px', 'jet_py', 'jet_pz', 'jet_energy', 'jet_mass']:
-        if len(all_test_data) == 0:
-            all_test_data = lfile[var_name]
-        else:
-            all_test_data = np.vstack((all_test_data, lfile[var_name]))
-
-    all_test_data = all_test_data.T
-
-
-    saveModel(model, train_data, all_test_data, lfile)
+    if options.testfile and  os.path.isfile(options.testfile):
+        test_file = pd.read_csv(options.testfile)
+        test_data = test_file[input_var_names]
+        savePrediction(model, test_data, test_file)
+ 
         
 #======================================================================================================        
 if __name__ == '__main__':
@@ -284,7 +496,7 @@ if __name__ == '__main__':
     log.info('Start job at %s:%s' %(socket.gethostname(), os.getcwd()))
     log.info('Current time: %s' %(time.asctime(time.localtime())))
     
-    main()
+    main_trainRNN()
 
     log.info('Local time: %s' %(time.asctime(time.localtime())))
     log.info('Total time: %.1fs' %(time.time()-timeStart))
